5d2d27d220a12d09cba0fe475922d5b583d16c39
Satenik <saten914@gmail.com>
2024-03-13 22:24:16 +0100

    Correción del main y registrarJugadores

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.List;
 
 public class  Main {
+    public static List<String> nombresJugadores = new ArrayList<>();
+    public static List<String> rolesJugadores = new ArrayList<>();
+    public static List<Boolean> estadoVivoJugadores = new ArrayList<>();
+    public static List<Integer> jugadoresReveladosPorElVidente = new ArrayList<>();
     public static Scanner scanner = new Scanner(System.in);
     public static Random random = new Random();
 
+    public static int enamorado1 = -1;
+    public static int enamorado2 = -1;
+    public static boolean cupidoHaActuado = false;
+
     public static void main(String[] args) {
-     Main juego=new Main(); //Creamos el objeto juego
-     juego.iniciarJuego();
+        System.out.println("Bienvenido al juego de Lobos y Aldeanos.");
     }
     public void iniciarJuego() {
         System.out.println("Bienvenidos al juego.");
@@ -17,12 +24,31 @@ public class  Main {
     }
 
     public void registrarJugadores() {
-        System.out.println("Introduce el numero de tus jugadores: ");
-        int numeroJugadores=Integer.parseInt(scanner.nextLine());
-        for (int i = 1; i <= numeroJugadores;i++) {
-            System.out.println("Nombre del jugador" + i + ":");
+        System.out.println("Introduce el número de jugadores:");
+        int numeroJugadores = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < numeroJugadores; i++) {
+            System.out.println("Nombre del jugador " + (i + 1) + ":");
             String nombre = scanner.nextLine();
+            nombresJugadores.add(nombre);
+            estadoVivoJugadores.add(true); // Todos los jugadores comienzan vivos
+            rolesJugadores.add(""); // Los roles están vacíos
         }
+    }
+    public void asignarRoles() {
+        int numeroJugadores = jugadores.size();
+        List<String> rolesDisponibles = new ArrayList();
+        rolesDisponibles.add("Cupido");
+        rolesDisponibles.add("Vidente");
+
+        int i;
+        for (i =0; i < 2; i++) {
+            rolesDisponibles.add("Lobo");
+        }
+        for(i = rolesDisponibles.size(); i < numeroJugadores; i++) {
+            rolesDisponibles.add ("Aldeano");
+        }
+        Iterator var6 = jugadores.iterator();
     }
     public void ejecutarFaseNocturna() {
         System.out.println("Coomienza la fase nocturna. ");
@@ -42,20 +68,20 @@ public class  Main {
             System.out.println("Lobos, elijan a quién quieren atacar: ");
             var2 = jugadores.iterator();
 
-            while(var2.hasNext()) {
-                jugador = (Jugador)var2.next();
+            while (var2.hasNext()) {
+                jugador = (Jugador) var2.next();
                 if (jugador.vivo && !jugador.rol.equals("Lobo")) {
-                    System.out.println(jugador.id+ ": "+ jugador.nombre);
+                    System.out.println(jugador.id + ": " + jugador.nombre);
                 }
             }
             int victimaId = scanner.nextInt();
             boolean esAldeano = false;
-            Iterator var4 =jugadores.iterator();
+            Iterator var4 = jugadores.iterator();
 
             while (var4.hasNext()) {
-                Jugador jugador = (Jugador)var4.next();
+                Jugador jugador = (Jugador) var4.next();
                 if (jugador.id == victimaId && !jugador.rol.equals("Lobo")) {
-                    jugador.vivo =false;
+                    jugador.vivo = false;
                     esAldeano = true;
                     System.out.println(jugador.nombre + "ha sido eliminada. Su rol era: " + jugador.rol);
                     break;
@@ -67,5 +93,18 @@ public class  Main {
             }
         }
 
+        static class Jugador {
+            int id;
+            String nombre;
+            String rol;
+            boolean vivo =true;
+            public Jugador (int id, String nombre ) {
+                this.id = id;
+                this.nombre =nombre;
+            }
+            public void setRol(String rol) {
+                this.rol = rol;
+            }
+        }
     }
 }
